78c4bf0364a0d9b0ef0f672363e6d960de480bfc
Brook Pan <pstily@126.com>
2018-03-05 07:29:48 -0800

    Initial the maven project.

---
+++ bigidiot/src/main/java/com/brpan/bigidiot/App.java
@@ -0,0 +1,13 @@
+package com.brpan.bigidiot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
