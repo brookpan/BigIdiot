7d2be984285d290785a1040f1f07d7ee23f678f6
Brook Pan <pstily@126.com>
2018-03-07 20:41:45 -0800

    Log framework build.

--- bigidiot/src/main/java/com/brpan/bigidiot/App.java
+++ bigidiot/src/main/java/com/brpan/bigidiot/App.java
@@ -1,13 +1,24 @@
 package com.brpan.bigidiot;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static Log logger = LogFactory.getLog(App.class);
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        logger.trace("trace message");
+        logger.debug("debug message");
+        logger.info("info message");
+        logger.warn("warn message");
+        logger.error("error message");
+        logger.fatal("fatal message");
     }
 }
