f08f1491b820bfba89292ac72b69bc77ec8e32c3
Brook Pan <pstily@126.com>
2018-04-04 17:35:58 -0700

    Closed Hibernate sessionfactory and session.

--- bigidiot/src/main/java/com/brpan/dao/impl/UserDaoImpl.java
+++ bigidiot/src/main/java/com/brpan/dao/impl/UserDaoImpl.java
@@ -48,6 +48,9 @@ public class UserDaoImpl implements UserDao {
 	      }finally {
 	         session.close(); 
 	      }	
+	      
+	      HibernateUtil.close(session);
+	      HibernateUtil.closeSessionFactory();
 		
 		return rtusers;
 	}
